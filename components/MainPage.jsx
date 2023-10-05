dddb419a2c807d8b0e3f56a861f4e44bb3ad8436
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-05 18:41:01 +0300

    style + ThemeComponent

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,22 +1,18 @@
 const React = require('react');
 const Layout = require('./Layout');
-
+const ThemeCard = require('../components/ThemeCard')
  
-function Home({ title }) {
+function Home({ title , themes = []}) {
   return (
     <Layout title={title} >
      <div className="start-page">
       <h1 className="start-page-title">QViz</h1>
       <div className="start-page-link-container">
-        <a href="#" className="start-page-link">
-          Тема 1
-        </a>
-        <a href="#" className="start-page-link">
-          Тема 2
-        </a>
-        <a href="#" className="start-page-link">
-          Тема 3
-        </a>
+      <div className="themeCard__container flex flex-dir-col align-item-center">
+                {themes.map((theme) => (
+                    <ThemeCard theme={theme} />
+                ))}
+            </div>
       </div>
     </div>
     </Layout>
---
+++ components/ThemeCard.jsx
@@ -0,0 +1,11 @@
+const React = require("react");
+
+function ThemeCard ({ theme }) {
+  return (
+    <div className="m-50">
+          <a href="#" className="start-page-link">{theme.title}</a>
+    </div>
+  );
+}
+
+module.exports = ThemeCard;
