4559e9a38d57dff6caa7fa0a71d0a3c7ab2fb1b9
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2023-10-05 18:58:53 +0300

    div fix

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -9,14 +9,13 @@ function Home({ title , themes = []}) {
      <div className="start-page">
         <h1 className="start-page-title">QViz</h1>
         <div className="start-page-link-container">
-        <div className="themeCard__container flex flex-dir-col align-item-center">
-                  {themes.map((theme) => (
-                      <ThemeCard theme={theme} />
-                  ))}
-              </div>
-
+          <div className="themeCard__container flex flex-dir-col align-item-center">
+                    {themes.map((theme) => (
+                        <ThemeCard theme={theme} />
+                    ))}
+          </div>
         </div>
-      </div>
+    </div>
     </Layout>
   );
 }
