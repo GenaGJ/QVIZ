4f7d387653738cd61be76c2936a92b1fd3856c92
GenaGJ <ivanm33333@mail.ru>
2023-10-06 12:11:32 +0300

    index

--- routes/themes.routes.js
+++ routes/themes.routes.js
@@ -6,8 +6,14 @@ router.get("/:themeId/question/:questionIndex", async (req, res) => {
     try {
         const { themeId, questionIndex } = req.params;
         const questions = await Question.findAll({ where: { theme_id: themeId} });
-        const html = res.renderComponent(Card, { title: 'card page', question: questions[questionIndex]});
-        res.send(html);
+        if(+questionIndex + 1 > questions.length){
+          res.redirect('/')
+        }
+        else{
+          const html = res.renderComponent(Card, { title: 'card page', question: questions[questionIndex], questionIndex});
+          res.send(html);
+          
+        }
       } catch ({ message }) {
         res.json({ message });
       }
